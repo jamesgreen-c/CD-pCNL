6edd68da64c16b668cc0ac246880e1e55d044268
jamesgreen-c <jamesenricodavidgreen@gmail.com>
2026-03-23 15:45:55 +0000

    Add bridge.py to simulate from bridge SDEs. Add brownian.py to exactly simulate Brownian motion

---
+++ cd_ssm/bridge.py
@@ -0,0 +1,49 @@
+from typing import Callable
+
+from chex import Array, PRNGKey
+import jax.random as jr
+import jax.numpy as jnp
+from jax import vmap, lax
+
+
+def drift(t, x, dt, xT):
+    return (xT - x) / (dt - t)
+
+
+def euler(
+        diffusion: Callable,
+        ws: Array,
+        x0: Array,
+        xT: Array,
+        t: Array,
+    ):
+    """
+    Implementation of the euler-maruyama scheme for approximating solutions to SDEs when the driving noise is given
+    
+    Parameters
+    ----------
+    diffusion:  The diffusion function. Should take (t, x) as args
+    ws:         The driving Brownian motion. Shape (num, N, dw, ...)
+    x0:         The starting point. Shape (N, d, ...) for N particles.
+    xT:         The end point. Shape (N, d, ...) for N particles.
+    t0:         The starting time
+    t:          The amount of time that passes
+    num:        The number of steps to take in dt time
+    N:          The number of particles
+    
+    Returns
+    ----------
+    xs:         The resulting interpolated paths (N, d, ..., num)
+    """
+    num = ws.shape[0]
+    dt = t / num
+    ts = jnp.arange(num) * dt
+
+    def _body(x_k, inps):
+        t_k, w_k = inps
+        x_k_p_1 = x_k + drift(t_k, x_k, dt, xT)*dt + diffusion(t_k, x_k) * jnp.sqrt(dt) * w_k
+        return x_k_p_1, x_k_p_1
+    
+    _, xs = lax.scan(_body, x0, (ts, ws))
+    xs = jnp.insert(xs, 0, x0, axis=0)
+    return xs
---
+++ cd_ssm/brownian.py
@@ -0,0 +1,41 @@
+from typing import Callable
+
+from chex import Array, PRNGKey
+import jax.random as jr
+import jax.numpy as jnp
+from jax import vmap, lax
+
+
+def simulate(
+        key: PRNGKey, 
+        w0: Array, 
+        t: Array,
+        num: Array,
+        N: Array
+    ):
+    """
+    Implementation of an exact simulate for standard Brownian motion
+    
+    Parameters
+    ----------
+    key:        RNG
+    w0:         The starting point. Shape (N', d, ...) for N particles. 
+    t:          The amount of time that passes
+    num:        The number of steps to take in dt time
+    N:          The number of particles
+    
+    Returns
+    ----------
+    ws:         The resulting Brownian motion (N, d, ..., num)
+    """
+
+    eps = jr.normal(key, (num, N, *w0.shape[1:]))
+    dt = t / num
+
+    def _body(w_k, eps_k):
+        w_k_p_1 = w_k + jnp.sqrt(dt) * eps_k
+        return w_k_p_1, w_k_p_1
+
+    _, ws = lax.scan(_body, w0, eps)
+    ws = jnp.insert(ws, 0, w0, axis=0)
+    return ws
