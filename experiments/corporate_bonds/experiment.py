33a81ff8ee3bff17bc5721887f8abdfb4ba10cf9
jamesgreen-c <jamesenricodavidgreen@gmail.com>
2026-06-16 16:42:30 +0100

    Experiment file for gueant csmc on corporate bonds

---
+++ experiments/corporate_bonds/experiment.py
@@ -0,0 +1,316 @@
+import argparse
+import os
+import time
+
+import jax
+import jax.numpy as jnp
+from jax.tree_util import tree_map
+from jax.scipy.special import logsumexp
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+from experiments.corporate_bonds.kernels import KernelType, get_gueant_csmc_kernel
+from experiments.corporate_bonds.model import get_data, get_half_spread_dynamics, get_ytb_dynamics
+
+from cd_ssm import bridge
+from cd_ssm.utils.common import force_move, barker_move
+from cd_ssm.utils.kalman import sampling, filtering
+from cd_ssm.utils.resamplings import killing, multinomial
+from cd_ssm.utils.timing import block_until_ready_tree
+
+import tensorflow_probability.substrates.jax as tfp
+
+# jax.config.update("jax_enable_x64", False)
+# jax.config.update("jax_platform_name", "cpu")
+
+# --- general config ---
+MIN_DELTA = 1e-12
+MAX_DELTA = 1e1
+MIN_RHO = 1e-5
+MAX_RHO = 1 - 1e-5
+DELTA_MIN_RATE = 1e-3
+RHO_MIN_RATE = 1e-6
+ADAPTATION_WINDOW = 100
+DELTA_ADAPTATION_RATE = 0.5
+RHO_ADAPTATION_RATE = 0.025
+
+# ARGS PARSING
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--T", dest="T", type=int, default=10)
+parser.add_argument("--D", dest="D", type=int, default=1)
+parser.add_argument("--K", dest="K", type=int, default=1)
+parser.add_argument("--M", dest="M", type=int, default=5)
+
+parser.add_argument("--log-var", dest="log_var", type=float, default=0)
+parser.add_argument("--phi", dest="phi", type=float, default=0.8)
+
+parser.add_argument("--steps", type=int, default=100)
+parser.add_argument("--mesh-num", dest="mesh_num", type=int, default=10)
+
+parser.add_argument("--adaptation", dest="adaptation", type=int, default=500)
+parser.add_argument("--burnin", dest="burnin", type=int, default=0)
+parser.add_argument("--n-samples", dest="n_samples", type=int, default=1000)
+
+parser.add_argument("--rho", dest="rho", type=float, default=.25)
+parser.add_argument("--rho-scale", dest="rho_scale", type=float, default=1/5)
+parser.add_argument("--rho-arg", dest="rho_arg", type=str, default="D")
+
+parser.add_argument("--delta", dest="delta", type=float, default=1)
+parser.add_argument("--delta-scale", dest="delta_scale", type=float, default=1)
+parser.add_argument("--delta-arg", dest="delta_arg", type=str, default="D")
+
+parser.add_argument("--target", dest="target", type=int, default=75)
+parser.add_argument("--target-stat", dest='target_stat', type=str, default="mean")
+
+parser.add_argument("--seed", dest="seed", type=int, default=1234)
+parser.add_argument("--kernel", dest="kernel", type=int, default=KernelType.CSMC)
+parser.add_argument("--style", dest="style", type=str, default="guided")
+
+parser.add_argument("--backward", action='store_true')
+parser.add_argument('--no-backward', dest='backward', action='store_false')
+parser.set_defaults(backward=True)
+
+parser.add_argument("--resampling", dest='resampling', type=str, default="multinomial")
+parser.add_argument("--last-step", dest='last_step', type=str, default="barker")
+parser.add_argument("--N", dest="N", type=int, default=31)  # total number of particles is N + 1
+
+parser.add_argument("--debug", action='store_true')
+parser.add_argument('--no-debug', dest='debug', action='store_false')
+parser.set_defaults(debug=False)
+
+parser.add_argument("--verbose", action='store_true')
+parser.add_argument('--no-verbose', dest='verbose', action='store_false')
+parser.set_defaults(verbose=False)
+
+parser.add_argument("--plot", action='store_true')
+parser.add_argument('--no-plot', dest='plot', action='store_false')
+parser.set_defaults(plot=False)
+
+args = parser.parse_args()
+
+print(f"""
+##################################
+#        LGSSM EXPERIMENT        #
+##################################
+Configuration:
+    - T:         {args.T}
+    - kernel:    {KernelType(args.kernel).name}
+    - style:     {args.style}
+    - D:         {args.D}
+    - N-samples  {args.n_samples}
+    - Adaptation {args.adaptation}
+    - Burnin     {args.burnin}
+""")
+
+# BACKEND CONFIG
+NOW = time.time()
+
+# PARAMETERS
+KEY = jax.random.PRNGKey(args.seed)
+ALL_KEYS = jax.random.split(KEY, args.K + 1)
+WARMUP_KEY = ALL_KEYS[0]
+EXPERIMENT_KEYS = ALL_KEYS[1:]
+
+kernel_type = KernelType(args.kernel)
+SHARED_DELTA = kernel_type.shared_delta()
+SHARED_RHO = kernel_type.shared_rho()
+
+# DELTA AND RHO CONFIG
+if args.delta_arg == "D":
+    DELTA = args.delta / args.D ** args.delta_scale
+elif args.delta_arg == "T":
+    DELTA = args.delta / args.T ** args.delta_scale
+elif args.delta_arg == "DT" or args.delta_arg == "TD":
+    DELTA = args.delta / (args.D * args.T) ** args.delta_scale
+else:
+    DELTA = args.delta
+
+if kernel_type.is_random_walk:
+    # overwrite rho config
+    RHO = DELTA / args.mesh_num
+    MIN_RHO = MIN_DELTA / args.mesh_num
+    MAX_RHO = MAX_DELTA
+    RHO_MIN_RATE = DELTA_MIN_RATE
+    RHO_ADAPTATION_RATE = DELTA_ADAPTATION_RATE
+    RHO_DIRECTION = +1
+
+else:
+    RHO_DIRECTION = -1
+    rho_m1 = 1 - args.rho
+    if args.rho_arg == "D":
+        RHO = 1 - (rho_m1 / args.D ** args.rho_scale)
+    elif args.rho_arg == "T":
+        RHO = 1 - (rho_m1 / args.T ** args.rho_scale)
+    elif args.rho_arg == "DT" or args.rho_arg == "TD":
+        RHO = 1 - (rho_m1 / (args.D * args.T) ** args.rho_scale)
+    else:
+        RHO = args.rho
+
+print(f"""
+ADAPTATION CONFIG:        
+    - delta init:                {DELTA}
+    - min/max delta:             {MIN_DELTA}/{MAX_DELTA}
+    - delta adaptation rate:     {DELTA_ADAPTATION_RATE}
+    - rho init:                  {RHO}
+    - min/max rho:               {MIN_RHO}/{MAX_RHO}
+    - rho adaptation rate:       {RHO_ADAPTATION_RATE}
+    - rho adaptation direction:  {RHO_DIRECTION}
+""")
+
+
+if args.resampling == "killing":
+    resampling_fn = killing
+elif args.resampling == "multinomial":
+    resampling_fn = multinomial
+else:
+    raise ValueError(f"Unknown resampling {args.resampling}")
+
+if args.last_step == "forced":
+    last_step_fn = force_move
+elif args.last_step == "barker":
+    last_step_fn = barker_move
+else:
+    raise ValueError(f"Unknown last step {args.last_step}")
+
+
+# --- target acceptance rate ---
+TARGET_ALPHA = args.target / 100 
+if args.target_stat.isnumeric():
+    TARGET_STAT = float(args.target_stat) / 100
+else:
+    TARGET_STAT = args.target_stat
+
+# --- dynamics config ---
+A = args.phi * jnp.eye(args.D)
+CHOL_P0_Z = 0.1 * jnp.eye(args.D)
+CHOL_P0_ETA = 0.1 * jnp.eye(args.D)
+CHOL_Q_Z = 10 ** (args.log_var / 2) * jnp.eye(args.D)  # independent spreads
+CHOL_Q_ETA = 0.1 * jax.random.normal(jax.random.PRNGKey(913123), (args.D, args.D))
+
+PSI = None
+CHOL_R = 0.1 * jnp.eye(args.D)
+ALPHA = None
+
+DTs = jnp.repeat(args.T / args.steps, args.steps)
+Ts = jnp.cumsum(DTs)
+DRIFT_z, DIFFUSION_z = get_half_spread_dynamics(A, CHOL_Q_Z)
+DRIFT_eta, DIFFUSION_eta = get_ytb_dynamics()
+OBS_SIGMA = 0.2
+
+# --- path reconstructor ---
+def to_path(us, es, diffusion):
+    """
+    us: (n_samples, M, steps, mesh, D)
+    es: (n_samples, M, steps, D)
+    """
+    @jax.vmap
+    def _to_path(_us, _es):
+        """
+        _us: (M, steps, mesh, D)
+        _es: (M, steps, D)
+        """
+        _reconstructor = jax.vmap(lambda u, ep, e, t, dt: bridge.to_path(diffusion, u, ep, e, t, dt))
+        return _reconstructor(_us[1:], _es[:-1], _es[1:], Ts[1:], DTs[1:])
+    
+    return _to_path(us, es)
+
+to_path_z = jax.vmap(lambda _us, _es: to_path(_us, _es, DIFFUSION_z))
+to_path_eta = jax.vmap(lambda _us, _es: to_path(_us, _es, DIFFUSION_eta))
+
+
+# ------- experiment function -------
+
+@(jax.jit if not args.debug else lambda x: x)
+def one_experiment(key):
+    data_key, sample_key = jax.random.split(key)
+
+    true_xs, obs, *_ = get_data(
+        data_key, args.D, DTs, 
+        A, PSI, CHOL_P0_Z, CHOL_P0_ETA, CHOL_Q_Z, CHOL_Q_ETA, CHOL_R, ALPHA
+    )
+
+    csmc_kernel, csmc_init, *_ = get_gueant_csmc_kernel(
+        obs, A, PSI, CHOL_P0_Z, CHOL_P0_ETA, CHOL_Q_Z, CHOL_Q_ETA, CHOL_R,
+        N=args.N, dts=DTs,
+        resampling_func=resampling_fn,
+        backward=True,
+        ancestor_move_func=last_step_fn,
+        style="guided", 
+        conditional=False
+    )
+    init_state = csmc_init(true_xs)   # no leakage as conditional = False
+
+    def _independent_sample(k_):
+        return csmc_kernel(k_, init_state)
+
+    sample_keys = jax.random.split(sample_key, args.M)
+    samples = jax.vmap(_independent_sample)(sample_keys)
+
+    return samples, true_xs, obs 
+
+# Compile once, without executing a full experiment
+start = time.time()
+compiled_one_experiment = one_experiment.lower(WARMUP_KEY).compile()
+print(f"Compile time: {time.time() - start:.2f} seconds.")
+
+# storage
+zs_all = np.empty((args.K, args.T, args.M, args.D))
+etas_all = np.empty((args.K, args.T, args.M, args.D))
+
+true_zs_all = np.empty((args.K, args.T, args.D))
+true_etas_all = np.empty((args.K, args.T, args.D))
+
+bond_indices_all = np.empty((args.K, args.T))
+event_types_all = np.empty((args.K, args.T))
+alphas_all = np.empty((args.K, args.T))
+obs_values_all = np.empty((args.K, args.T))
+
+for k, key_k in enumerate(tqdm.tqdm(EXPERIMENT_KEYS, desc="Experiment: ")):
+    samples_k, true_xs_k, obs_k = compiled_one_experiment(key_k)
+
+    zs, etas = samples_k
+    true_zs, true_etas = true_xs_k
+    bond_indices, event_types, alphas, obs_values = obs_k
+
+    zs_all[k] = zs
+    etas_all[k] = etas
+    true_zs_all[k] = true_zs
+    true_etas_all[k] = true_etas
+    bond_indices_all[k] = bond_indices
+    event_types_all[k] = event_types
+    alphas_all[k] = alphas
+    obs_values_all[k] = obs_values
+
+if not os.path.exists("results"):
+    os.mkdir("results")
+
+experiment_name = "kernel={},style={},D={},T={},N={},mesh-num={},steps={},M={},s={},a={},b={},seed={}"
+experiment_name = experiment_name.format(
+    kernel_type.name,
+    args.style,
+    args.D,
+    args.T,
+    args.N,
+    args.M,
+    args.seed,
+)
+
+dirpath = f"results/{experiment_name}"
+if not os.path.exists(dirpath):
+    os.mkdir(dirpath)
+
+datapath = f"{dirpath}/data.npz"
+np.savez_compressed(
+    datapath,
+    zs=zs_all,
+    etas=etas_all,
+    true_zs=true_zs_all,
+    true_etas=true_etas_all,
+    bond_indices=bond_indices_all,
+    event_types=event_types_all,
+    alphas=alphas_all,
+    obs_values=obs_values_all
+)
